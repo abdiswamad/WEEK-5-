01d3b2c122f306b5913393a034c3d8126b0f9e9d
abdiswamad <abdisamadmohamud@students.zetech.ac.ke>
2026-03-08 09:08:54 +0300

    Add files via upload

---
+++ calculateElectricBill.c
@@ -0,0 +1,30 @@
+//Program for calculating Electric Bill
+//ABDISAMAD ALI 
+//BCS-03-0001/2026
+
+#include <stdio.h>
+
+float calculateElectricBill(int units) {
+    float bill;
+
+    if (units <= 100) {
+        bill = units * 10;
+    } 
+    else if (units <= 200) {
+        bill = (100 * 10) + (units - 100) * 15;
+    } 
+    else {
+        bill = (100 * 10) + (100 * 15) + (units - 200) * 20;
+    }
+
+    return bill;
+}
+
+int main() {
+    int units;
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    printf("Total bill: KSh %.2f\n", calculateElectricBill(units));
+    return 0;
+}
